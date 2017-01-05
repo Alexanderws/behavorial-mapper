b3b7547c04b8a7a54b9456c19c1219293ed11c4e
Alexander <alexander@alexanders-macbook-pro.local>
2017-01-05 09:07:40 +0100

    Initial Commit

---
+++ Behavorial Mapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Behavorial Mapper
+//
+//  Created by Alexander on 05/01/2017.
+//  Copyright © 2017 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
