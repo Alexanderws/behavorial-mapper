10b3f7519b935b5bc288e71f40d2702b2f01e281
Alexander <alexander@alexanders-macbook-pro.local>
2017-01-17 09:19:50 +0100

    Added Style.swift

---
+++ Behavorial Mapper/Functions.swift
@@ -0,0 +1,24 @@
+//
+//  Functions.swift
+//  Behavorial Mapper
+//
+//  Created by Alexander on 12/01/2017.
+//  Copyright © 2017 Alexander. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+    func getWhiteBackground(width: CGFloat, height: CGFloat) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: width, height: height)
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: width, height: height), false, 0)
+        UIColor.white.setFill()
+        UIRectFill(rect)
+        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return image
+    }
+    
+    
+    
